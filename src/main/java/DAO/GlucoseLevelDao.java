9f18c1d7f0e722788473c5c0163222b010fd7a55
I M A N E <imanebahy91@gmail.com>
2024-06-03 14:41:23 +0100

    third commit

--- src/main/java/DAO/GlucoseLevelDao.java
+++ src/main/java/DAO/GlucoseLevelDao.java
@@ -1,4 +1,12 @@
 package DAO;
 
+import Models.GlucoseLevel;
+
+import java.util.List;
+
 public interface GlucoseLevelDao {
+    void addGlucoseLevel(GlucoseLevel glucoseLevel);
+    void deleteGlucoseLevel(int glucoseLevelId);
+    GlucoseLevel getGlucoseLevel(int glucoseLevelId);
+    List<GlucoseLevel> getAllGlucoseLevels();
 }
--- src/main/java/DAO/PatientDao.java
+++ src/main/java/DAO/PatientDao.java
@@ -1,4 +1,11 @@
 package DAO;
 
+import Models.Patient;
+
+import java.util.List;
+
 public interface PatientDao {
+    void addPatient(Patient patient);
+    void deletePatient(Patient patient);
+
 }
