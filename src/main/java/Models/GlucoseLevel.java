f95614dfb483c889d9574145c0fdc7ef20a72ad0
I M A N E <imanebahy91@gmail.com>
2024-06-03 14:36:51 +0100

    second commit

--- src/main/java/Models/GlucoseLevel.java
+++ src/main/java/Models/GlucoseLevel.java
@@ -1,4 +1,52 @@
 package Models;
 
+import java.time.LocalDateTime;
+
 public class GlucoseLevel {
+    private Integer gId;
+    private LocalDateTime dateTime;
+    private Double glucoseLevel;
+    private Patient patient;
+
+    public GlucoseLevel() {
+    }
+
+    public GlucoseLevel(Integer gId, LocalDateTime dateTime, Double glucoseLevel, Patient patient) {
+        this.gId = gId;
+        this.dateTime = dateTime;
+        this.glucoseLevel = glucoseLevel;
+        this.patient = patient;
+    }
+
+    public Integer getgId() {
+        return gId;
+    }
+
+    public void setgId(Integer gId) {
+        this.gId = gId;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Double getGlucoseLevel() {
+        return glucoseLevel;
+    }
+
+    public void setGlucoseLevel(Double glucoseLevel) {
+        this.glucoseLevel = glucoseLevel;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
 }
--- src/main/java/Models/Patient.java
+++ src/main/java/Models/Patient.java
@@ -1,4 +1,62 @@
 package Models;
+import java.time.LocalDateTime;
+
 
 public class Patient {
+    private int pId;
+    private String Name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    public Patient() {
+    }
+
+    public Patient(int pId, String name, String email, String phoneNumber, String address) {
+        this.pId = pId;
+        Name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public int getpId() {
+        return pId;
+    }
+
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
